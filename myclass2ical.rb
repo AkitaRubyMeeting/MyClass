bbd40ad96d050ccfeea2ba4d160ee0ba2bb7c360
Katsuyoshi Ito <kito@itosoft.com>
2014-05-13 23:01:55 +0900

    remove memo & change extension ical to ics

--- myclass2ical.rb
+++ myclass2ical.rb
@@ -46,17 +46,5 @@ def make_ical events
 end
 
 s = make_ical get_akitam_events
-
-File.open("akitam.ical", "w"){|f| f.write(s)}
-
-__END__
-cal = Icalendar::Calendar.new
-cal.event do |e|
-  e.dtstart     = Icalendar::Values::Date.new('20050428')
-  e.dtend       = Icalendar::Values::Date.new('20050429')
-  e.summary     = "Meeting with the man."
-  e.description = "Have a long lunch meeting and decide nothing..."
-  e.ip_class    = "PRIVATE"
-end
-
-p cal.to_ical
+p s
+File.open("akitam.ics", "w"){|f| f.write(s)}
