d4c95dd1f0fa23a880072d068c9ad266c96ad793
Katsuyoshi Ito <kito@itosoft.com>
2014-05-13 23:57:30 +0900

    refactoring

--- myclass2ical.rb
+++ myclass2ical.rb
@@ -10,20 +10,20 @@ def get_akitam_events
 
   # イベントは.veventで取得できる
   doc.search(".vevent").map do |event_info|
-    event = {}
-  
+    
     # 日付は.event_dateで取得
     a = event_info
           .search(".event_date").inner_text.gsub("\r\n", "")
           .scan(/(\d+)年(\d+)月(\d+)日/)
           .flatten
           .map{|e| e.to_i}
-    event[:date] = Date.new(*a)
+    date = Date.new(*a)
   
     # イベント名は.event_nameで取得
-    event[:name] = event_info
+    name = event_info
                     .search(".event_name").inner_text
-    event
+
+    { date:date, name:name }
   end
 end
 
