f361198e508343eb76bc94e11362e5c5cacec8b2
Katsuyoshi Ito <kito@itosoft.com>
2014-05-18 22:25:35 +0900

    rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mybenkyoukai::Application.config.secret_key_base = '76476d177b728865c6e7895dcdb14326da0cfc94c09bf7c75bd5aad49b6bb1ebd968ebb23bbb63a2cce85bc864063024197e375ea908b92c790d2fcc66da0bdc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mybenkyoukai::Application.config.session_store :cookie_store, key: '_mybenkyoukai_session'
